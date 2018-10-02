1425fc79a6be965c2d5a99db6b2cb9de3c921063
Amarnath <30647337+Amarmuddana@users.noreply.github.com>
2018-10-01 22:40:10 -0400

    Add files via upload

---
+++ Day1/scratch_day1.py
@@ -0,0 +1,23 @@
+#understanding how to write a main function in different ways in python and how to pass arguments
+#example 1
+print(__name__)
+
+
+#example 2
+def main():
+    print("Hello World!")
+
+
+main()
+print("test1")
+
+#exampe 3
+
+def main():
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
+
+print("test")
