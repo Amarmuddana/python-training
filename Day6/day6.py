2ab6a5f0a35f08db2a6b1c4c70516bb8b00da16d
Amarnath <30647337+Amarmuddana@users.noreply.github.com>
2018-10-06 20:54:43 -0400

    Add files via upload

---
+++ Day6/day6.py
@@ -0,0 +1,32 @@
+# Making Strings Upper and Lower Case
+string = "Welcome World"
+print(string.upper())
+
+output
+WELCOME WORLD
+
+#Example 2
+string = "WELCOME WORLD"
+print(string.lower())
+
+output
+welcome world
+
+#Example 3
+text = 'Love the world'
+
+# splits at space
+print(text.split())
+
+list = 'Milk, Chicken, Bread'
+
+# splits at ','
+print(list.split(', '))
+
+# Splitting at ':'
+print(list.split(':'))
+
+output for all three programs
+['Love', 'the', 'world']
+['Milk', 'Chicken', 'Bread']
+['Milk, Chicken, Bread']
