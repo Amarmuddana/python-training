2e7803a2ac06c7f480a3dc20e0f66ab09cbc45cc
Amarnath <30647337+Amarmuddana@users.noreply.github.com>
2018-10-13 22:21:17 -0400

    Add files via upload

---
+++ Day12/day12.py
@@ -0,0 +1,29 @@
+#Output Using print() function
+
+print('This sentence is output to the screen')
+# Output: This sentence is output to the screen
+
+a = 5
+
+print('The value of a is', a)
+# Output: The value of a is 5
+
+# object
+print(1,2,3,4)
+# Output: 1 2 3 4
+
+print(1,2,3,4,sep='*')
+# Output: 1*2*3*4
+
+print(1,2,3,4,sep='#',end='&')
+# Output: 1#2#3#4&
+
+
+#input with number
+
+num = input('Enter a number: ')
+Enter a number: 10
+print('num')
+
+
+#Output :'10'
