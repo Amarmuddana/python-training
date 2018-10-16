df44a7a05b5f812d93c4d22ea7a7d7890039542d
Amarnath <30647337+Amarmuddana@users.noreply.github.com>
2018-10-15 21:32:48 -0400

    Add files via upload

---
+++ Day13/day13.py
@@ -0,0 +1,44 @@
+#Arithmetic operators in Python
+x = 15
+y = 4
+
+print('x + y =',x+y)
+# Output: x + y = 19
+
+print('x - y =',x-y)
+# Output: x - y = 11
+
+print('x * y =',x*y)
+# Output: x * y = 60
+
+print('x / y =',x/y)
+# Output: x / y = 3.75
+
+print('x // y =',x//y)
+# Output: x // y = 3
+
+print('x ** y =',x**y)
+# Output: x ** y = 50625
+
+#Comparison operators in Python
+x = 10
+y = 12
+
+print('x > y  is',x>y)
+# Output: x > y is False
+
+print('x < y  is',x<y)
+# Output: x < y is True
+
+print('x == y is',x==y)
+# Output: x == y is False
+
+print('x != y is',x!=y)
+# Output: x != y is True
+
+print('x >= y is',x>=y)
+# Output: x >= y is False
+
+print('x <= y is',x<=y)
+# Output: x <= y is True
+
